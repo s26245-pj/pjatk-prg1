8593eb7c73412d42b249a5f0657fe26eb272f586
Michał Jastrzemski <s26245@pjwstk.edu.pl>
2021-10-25 11:46:27 +0200

    S03-password

---
+++ src/s03-password.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+std::string password;
+auto main() -> int
+
+{
+
+
+    do
+    {
+    std::cout << "Password:";
+    std::cin >> password;
+    }
+
+    while(password!="student");
+
+
+    std::cout << "Password is correct!";
+
+
+    return 0;
+}
+
