9f938991a6ef00dba5f8dc986dda67ee45f72be6
Michał Jastrzemski <s26245@pjwstk.edu.pl>
2021-11-02 14:48:12 +0100

    countdown update

--- src/s03-countdown.cpp
+++ src/s03-countdown.cpp
@@ -10,7 +10,7 @@ auto main(int argc, char* argv[]) ->int
   }
    auto x=std::string{argv[1]};
    int begin=stoi(x);    
-   for (auto i=begin; i>=0; i--)
+   for(auto i=begin; i>=0; i--)
    {
    std::this_thread::sleep_for(std::chrono::milliseconds{1000});
    std::cout<<i<<"\n";
