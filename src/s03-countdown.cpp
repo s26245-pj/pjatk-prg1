502990b0aa06d2fe373ba08cd173a11d5f546f9f
Michał Jastrzemski <s26245@pjwstk.edu.pl>
2021-11-02 14:46:12 +0100

    coundown update

--- src/s03-countdown.cpp
+++ src/s03-countdown.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
+#include <string>
 #include <chrono>
 #include <thread>
 
-auto main() -> int
+auto main(int argc, char* argv[]) ->int
 {
-
-  for (int i=10; i>=0; i--)
-  {
+  if(argc==0){
+  return 1;
+  }
+   auto x=std::string{argv[1]};
+   int begin=stoi(x);    
+   for (auto i=begin; i>=0; i--)
+   {
    std::this_thread::sleep_for(std::chrono::milliseconds{1000});
-   std::cout <<i<< "\n";
+   std::cout<<i<<"\n";
    }
-    std::cout <<"Happy new year!" << "\n";
     return 0;
 }
 
