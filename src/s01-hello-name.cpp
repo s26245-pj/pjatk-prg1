719218b32495380e7fead787f182d499935fa92b
Michał Jastrzemski <s26245@pjwstk.edu.pl>
2021-11-02 09:12:17 +0100

    update

--- src/s01-hello-name.cpp
+++ src/s01-hello-name.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <string>
 
-auto main() -> int
+auto main(int argc, char* argv[]) ->int
 {
-  auto name=std::string{};
-  std::getline(std::cin, name);
-  
-  std::cout << "Hello, " << name << "!\n";
-  return 0;
-  
+   if (argc==0)
+ {
+   return 1;
+   }
+
+    auto const name=std::string{argv[1]};
+    std::cout<<"Hello, "<<name<<"!\n";
+    return 0;
 }
