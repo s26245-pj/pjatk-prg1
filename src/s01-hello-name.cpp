48244a7fbafbde092b1204dadcee9b99a54762ca
Michał Jastrzemski <s26245@pjwstk.edu.pl>
2021-11-02 09:13:53 +0100

    update

--- src/s01-hello-name.cpp
+++ src/s01-hello-name.cpp
@@ -4,10 +4,9 @@
 auto main(int argc, char* argv[]) ->int
 {
    if (argc==0)
- {
+  {
    return 1;
    }
-
     auto const name=std::string{argv[1]};
     std::cout<<"Hello, "<<name<<"!\n";
     return 0;
