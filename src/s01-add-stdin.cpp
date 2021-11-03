1dadb3440cfc0d2081ab32db71138577908fd032
Michał Jastrzemski <s26245@pjwstk.edu.pl>
2021-11-03 16:38:50 +0100

    calc

--- src/s01-add-stdin.cpp
+++ src/s01-add-stdin.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 #include <string>
 
-auto ask_user_for_integer(std::string prompt)->int
+auto ask_user_for_integer(std::string prompt) -> int
 {
-     std::cout<<prompt<<"int:";
-     auto n=std::string{};
-     std::getline(std::cin, n);
-     return std::stoi(n);
+   std::cout << prompt << "int:";
+   auto n = std::string{};
+   std::getline(std::cin, n);
+   return std::stoi(n);
 }
 
-auto main()->int
+auto main() -> int
 {
-   auto const a=ask_user_for_integer("a=");
-   auto const b=ask_user_for_integer("b=");
-   std::cout<<(a+b)<<"\n";
+   auto const a = ask_user_for_integer("a=");
+   auto const b = ask_user_for_integer("b=");
+   std::cout << (a + b) << "\n";
    return 0;
 }
