3c9d369d9803c7b2fbe8642fe87a3d79c79856cc
Michał Jastrzemski <s26245@pjwstk.edu.pl>
2021-11-03 18:13:13 +0100

    countdown

--- src/s03-countdown.cpp
+++ src/s03-countdown.cpp
@@ -3,18 +3,18 @@
 #include <chrono>
 #include <thread>
 
-auto main(int argc, char* argv[]) ->int
+auto main(int argc, char* argv[]) -> int
 {
-  if(argc==0){
-  return 1;
-  }
-   auto x=std::string{argv[1]};
-   int begin=stoi(x);    
-   for(auto i=begin; i>=0; i--)
-   {
-   std::this_thread::sleep_for(std::chrono::milliseconds{1000});
-   std::cout<<i<<"\n";
+   if (argc == 0){
+      return 1;
    }
-    return 0;
+   auto x = std::string{argv[1]};
+   int begin = stoi(x);
+    
+   for (auto i = begin; i >= 0; i--){
+       std::this_thread::sleep_for(std::chrono::milliseconds{1000});
+       std::cout << i << "\n";
+   }
+   return 0;
 }
 
