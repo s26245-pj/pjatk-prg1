af49d1cd02f60aa1d8fb6bea5ec4f57fc1979912
Michał Jastrzemski <s26245@pjwstk.edu.pl>
2021-11-07 17:54:52 +0100

    updt

--- src/s01-div.cpp
+++ src/s01-div.cpp
@@ -5,6 +5,9 @@ auto main(int, char* argv[]) -> int
 {
    auto const a = std::stoi(argv[1]);
    auto const b = std::stoi(argv[2]);
+   if (b == 0)
+   std::cout << "We do not divide by 0!" << "\n";
+   else
    std::cout << (a / b) << "\n";
    return 0;
 }
