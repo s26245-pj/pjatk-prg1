0f0d5b9b4005cf79f9ace9830a84fbabf4fa3ca0
Michał Jastrzemski <s26245@pjwstk.edu.pl>
2021-11-07 12:40:39 +0100

    argv

--- src/s01-hello-argv.cpp
+++ src/s01-hello-argv.cpp
@@ -2,12 +2,8 @@
 #include <string>
 
 
-auto main(int argc, char* argv[]) -> int
+auto main(int, char* argv[]) -> int
 {
-   if (argc == 0){
-   return 1;
-   }
-
    auto const name = std::string{argv[1]};
    std::cout << "Hello, " << name << "!\n";
 
