f979f7be8beb54a55b2b0093862a0b6a8035f586
Michał Jastrzemski <s26245@pjwstk.edu.pl>
2021-11-07 17:57:40 +0100

    updt

--- src/s01-div-stdin.cpp
+++ src/s01-div-stdin.cpp
@@ -13,6 +13,8 @@ auto main() -> int
 {
    auto const a = ask_user_for_integer("a=");
    auto const b = ask_user_for_integer("b=");
+   if (b == 0)
+   std::cout << "We do not divide by 0!" << "\n";
    std::cout << (a / b) << "\n";
    return 0;
 }
