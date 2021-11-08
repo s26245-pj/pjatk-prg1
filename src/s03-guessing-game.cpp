7655f1d0296665389101a3b725893278d8f01175
Michał Jastrzemski <s26245@pjwstk.edu.pl>
2021-11-08 16:57:20 +0100

    gg

--- src/s03-guessing-game.cpp
+++ src/s03-guessing-game.cpp
@@ -12,21 +12,25 @@ auto ask_user_for_integer(std::string prompt) -> int
 
 auto main() -> int
 {
+    auto attempt = 1;
     auto rnd = std::random_device{};
     auto d = std::uniform_int_distribution<int>{1, 100};
     auto number = d(rnd); 
     auto guess = ask_user_for_integer("Guess number from 1 to 100: ");
   
     while (guess != number){
-    if (guess < number)
-    std::cout <<"Too small. try again!\n";
-    else if (guess > number)
-    std::cout << "Too big, try again\n";
+    attempt++;
+    if (guess < number){
+    std::cout <<"Too small. try again, it's your " << attempt << " attempt\n";
+    }
+    else if (guess > number){
+    std::cout << "Too big, try again, it's your " << attempt << " attempt\n";
+    }
 
     guess = ask_user_for_integer("Guess number from 1 to 100: ");
     }
 
-    std::cout << "That's right, you won!\n";
+    std::cout << "That's right, you won in " << attempt << " attempt!" << "\n";
     
     return 0;
 }
