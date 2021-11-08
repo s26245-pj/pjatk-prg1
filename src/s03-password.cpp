dd1df157aea535c00f1dc933aa269d0f68d3846e
Michał Jastrzemski <s26245@pjwstk.edu.pl>
2021-11-08 13:38:23 +0100

    ss

--- src/s03-password.cpp
+++ src/s03-password.cpp
@@ -5,14 +5,14 @@
 auto ask_user_for_password(std::string prompt) -> std::string
 {
    auto line = std::string{};
-   std::cout << prompt << "Password: ";
+   std::cout << prompt;
    std::getline(std::cin, line);
    return line;
 }
    
 auto main(int, char* argv[]) -> int
 {  
-   auto a = ask_user_for_password;
+   auto a = ask_user_for_password("password: "); 
    while (a != argv[1]){
    a = ask_user_for_password("password: ");
    if (a != argv[1])
