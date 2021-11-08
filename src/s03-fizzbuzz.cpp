b88f3ca220b643b9d4015a42084e3b90dc7f59cc
Michał Jastrzemski <s26245@pjwstk.edu.pl>
2021-11-08 16:30:26 +0100

    fizzbuzz

--- src/s03-fizzbuzz.cpp
+++ src/s03-fizzbuzz.cpp
@@ -2,31 +2,24 @@
 
 auto ask_user_for_integer(std::string prompt) -> int
 {
-   std::cout << prompt << "Enter the amount of numbers: ";
+   std::cout << prompt;
    auto amount_of_numbers = std::string{};
    std::getline(std::cin, amount_of_numbers);
    return std::stoi(amount_of_numbers); 
 }
 
-auto main(int argc, char* argv[]) -> int
-
+auto main() -> int
 {
+   auto const a = ask_user_for_integer("Enter the amount of numbers: ");
 
-    for (auto i = 1; i<=n; i++)
-    {
-    if ((i%3==0 && i%5==0))
-    {
-        std::cout << i << " FizzBuzz" << "\n";
-    }
-    else if (i%3==0)
-    {
-        std::cout << i << " Fizz" << "\n";
-    }
-    else if (i%5==0)
-    {
-        std::cout << i << " Buzz" << "\n";
-    }
-    }
-    return 0;
+   for (auto i = 1; i <= a; i++){
+     if ((i % 3 == 0 && i % 5 == 0)) 
+       std::cout << i << " FizzBuzz\n";
+     else if (i % 3 == 0)  
+       std::cout << i << " Fizz\n";
+     else if (i % 5 == 0)    
+       std::cout << i << " Buzz\n"; 
+   }
+   return 0;
 }
 
