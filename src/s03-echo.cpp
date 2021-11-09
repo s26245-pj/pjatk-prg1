244882bada585dc8cdac02df840e5fa39f09f101
Michał Jastrzemski <s26245@pjwstk.edu.pl>
2021-11-09 12:12:01 +0100

    ech

--- src/s03-echo.cpp
+++ src/s03-echo.cpp
@@ -1,16 +1,18 @@
-# include <algorithm>
-# include <iostream>
-# include <iterator>
-# include <string>
-# include <vector>
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iterator>
 
 auto main (int argc, char* argv[]) -> int
-{
-   auto args = std::vector<std::string>{};
-   std::copy_n(argv, argc, std::back_inserter(args));
-   for (auto const& each : args)
-   {
-   std :: cout << each << "\n";}
-
-return 0;
+{    
+  auto args = std::vector<std::string>{};
+  std::copy_n(argv, argc, std::back_inserter(args));
+  
+  for (auto const& each_arg : args){
+  if (argv[1] = -n){
+  std::cout << each_arg << " ";
+  }
+  std::cout << "\n";
+  return 0;
 }
