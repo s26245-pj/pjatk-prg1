4a97efb026a55c09ab7b4aba4f65fb288cf8d708
Michał Jastrzemski <s26245@pjwstk.edu.pl>
2021-11-17 16:43:38 +0100

    ech

--- src/s03-echo.cpp
+++ src/s03-echo.cpp
@@ -7,12 +7,9 @@
 auto main (int argc, char* argv[]) -> int
 {    
   auto args = std::vector<std::string>{};
-  std::copy_n(argv, argc, std::back_inserter(args));
   
-  for (auto const& each_arg : args){
-  if (argv[1] = -n){
-  std::cout << each_arg << " ";
-  }
-  std::cout << "\n";
+  for (auto i =1; i < argc; i++){
+  args.push_back(argc);
+   }
   return 0;
 }
