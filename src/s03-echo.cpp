a9fd67f354d59833f3a4b2bee39ab61712a5b618
Michał Jastrzemski <s26245@pjwstk.edu.pl>
2021-11-18 16:51:44 +0100

    echo

--- src/s03-echo.cpp
+++ src/s03-echo.cpp
@@ -1,15 +1,49 @@
-#include <algorithm>
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 #include <iterator>
 
-auto main (int argc, char* argv[]) -> int
-{    
-  auto args = std::vector<std::string>{};
-  
-  for (auto i =1; i < argc; i++){
-  args.push_back(argc);
+
+auto main(int argc, char* argv[]) -> int
+{
+   std::string L = "-l";
+   std::string R = "-r";
+   std::string N = "-n";
+   auto args = std::vector<std::string>{};
+   std::copy_n(argv, argc, std::back_inserter(args));
+      
+   if (std::find(args.begin(), args.end(), R) != args.end()){
+      for (auto i = 1; i < argc; i++) {
+         std::copy_n(argv, argc, std::back_inserter(args));         
+         std::reverse(args.begin(), args.end());
+         std::cout << args[i] << " ";   
+      }
+      std::cout << "\n";
    }
-  return 0;
+
+   if (std::find(args.begin(), args.end(), L) != args.end()){
+      for (auto i = 1; i < argc; i++) {
+         std::copy_n(argv, argc, std::back_inserter(args));
+         std::cout << args[i] << "\n";   
+      }
+   }
+
+   if (std::find(args.begin(), args.end(), N) != args.end()){
+      for (auto i = 1; i < argc; i++) {
+         std::copy_n(argv, argc, std::back_inserter(args));
+         std::cout << args[i] << " ";   
+      }
+   }
+
+   else {
+      for (auto i = 1; i < argc; i++) {
+         std::cout << args[i] << " ";   
+      }
+      std::cout << "\n";
+   }  
+   
+   return 0;
 }
+
+
