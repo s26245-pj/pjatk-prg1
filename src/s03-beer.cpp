c46685430f809fa1ebd220326ab609371d56ab25
Michał Jastrzemski <s26245@pjwstk.edu.pl>
2021-11-18 12:02:26 +0100

    beer updt

--- src/s03-beer.cpp
+++ src/s03-beer.cpp
@@ -1,31 +1,37 @@
 #include <iostream>
 
-auto main(int argc, char* argv[]) -> int
+auto main(int argc, char *argv[]) -> int 
 {
    auto bottle_count = 99;
 
-   if (argc > 1){
+   if (argc > 1) {
       bottle_count = std::stoi(argv[1]);
-      if (bottle_count <= 0){
-         std::cout << "There's nothing on the wall...\n";
-      
+      if (bottle_count <= 0) {
+            std::cout << "There's nothing on the wall...\n";
+
       return 1;
       }
    }
-      
-   for (auto i = bottle_count; i > 1; i--){
-      std::cout << i << " bottles of beer on the wall\n"
-                << i << " bottles of beer\n"
-                << "take one down, pass it around\n"
-                << i - 1 << " bottles of beer on the wall\n";  
+
+   for (auto i = bottle_count; i > 0; i--) {
+      if (i > 1) {
+         std::cout << i << " bottle(s) of beer on the wall, " << i
+                   << " bottle(s) of beer,\n"
+                   << "take one down, pass it around, " << i - 1
+                   << " bottle(s) of beer on the wall,\n";
+      } 
+      else {
+         std::cout << i << " bottle of beer on the wall, " << i
+                   << " bottle of beer,\n"
+                   << "take one down, pass it around, "
+                   << "no more bottles of beer on the wall,\n";
+      }
    }
-      
-   std::cout << "1 bottle of beer on the wall\n"
-             <<  "1 bottle of beer\n"
-             << "take one down, pass it around\n"   
+
+   std::cout << "no more bottles of beer on the wall, "
              << "no more bottles of beer\n"
-             << "Go to the store and buy some more\n"
-             << bottle_count << " bottles of beer on the wall...\n";
+             << "go to the store and buy some more, " << bottle_count
+             << " bottles of beer on the wall...\n";
 
    return 0;
 }
