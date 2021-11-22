304ccc27df24135373f043b50b2d4d80fba5cac5
Michał Jastrzemski <s26245@pjwstk.edu.pl>
2021-11-22 11:32:27 +0100

    fzbz

--- src/s03-fizzbuzz.cpp
+++ src/s03-fizzbuzz.cpp
@@ -2,17 +2,18 @@
 
 auto main(int, char *argv[]) -> int 
 {
-  auto a = std::stoi(argv[1]);
+   auto a = std::stoi(argv[1]);
 
-  for (auto i = 1; i <= a; i++) {
-    if ((i % 3 == 0 && i % 5 == 0))
-      std::cout << i << " FizzBuzz\n";
-    else if (i % 3 == 0)
-      std::cout << i << " Fizz\n";
-    else if (i % 5 == 0)
-      std::cout << i << " Buzz\n";
-    else
-      std::cout << i << "\n";
-  }
-  return 0;
+   for (auto i = 1; i <= a; i++) {
+      std::cout << i << " "; 
+      if (i % 3 == 0 ) {
+         std::cout << "Fizz";
+      }
+      if (i % 5 == 0) {
+         std::cout << "Buzz";
+      }
+      std::cout << "\n";
+   }
+  
+   return 0;
 }
