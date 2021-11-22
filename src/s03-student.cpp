ed9c859cec1083917e84425ac0d6f1b29f306d09
Michał Jastrzemski <s26245@pjwstk.edu.pl>
2021-11-22 17:59:04 +0100

    student

--- src/s03-student.cpp
+++ src/s03-student.cpp
@@ -5,15 +5,20 @@ struct student {
    std::string student_name;
    std::string student_surname;
    std::string index_number;
+   int current_semester;
    float grades_average; 
-   
-} my_details;
+
+//auto student::to_string()   
+};
 
 
 auto main() -> int
 {
+   auto my_details;
+   //auto me = student {
+   
    my_details.student_name = "Michał";
    my_details.student_surname = "Jastrzemski";
    my_details.index_number = "s26245";
-
+   
 }
