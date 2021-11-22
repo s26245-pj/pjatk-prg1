91f5cfc646efdd7656c216971777764c0574eff3
Michał Jastrzemski <s26245@pjwstk.edu.pl>
2021-11-22 19:33:19 +0100

    added beginning of student struct

--- src/s03-student.cpp
+++ src/s03-student.cpp
@@ -1,24 +1,34 @@
 #include <iostream>
 #include <string>
 
-struct student {
+struct Student 
+{
    std::string student_name;
    std::string student_surname;
    std::string index_number;
    int current_semester;
    float grades_average; 
 
-//auto student::to_string()   
+   auto to_string() const -> std::string {
+      return ("Student name: " + student_name
+           + "\nStudent surname: " + student_surname
+           + "\nStudent index number: " + index_number
+           + "\nCurrent semester: " + std::to_string(current_semester)
+           + "\nGrades point average: " + std::to_string(grades_average)
+      );       
+   }
 };
 
 
 auto main() -> int
 {
-   auto my_details;
-   //auto me = student {
+   Student my_details;
    
    my_details.student_name = "Michał";
    my_details.student_surname = "Jastrzemski";
    my_details.index_number = "s26245";
-   
+   my_details.current_semester = 1;
+   my_details.grades_average = 4.57;
+
+   std::cout << my_details.to_string() << "\n";
 }
