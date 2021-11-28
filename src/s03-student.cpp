c4dcf12299aebd2492589866ceb936bf0af23cc4
Michał Jastrzemski <s26245@pjwstk.edu.pl>
2021-11-28 18:30:41 +0100

    student updt

--- src/s03-student.cpp
+++ src/s03-student.cpp
@@ -1,34 +1,30 @@
 #include <iostream>
 #include <string>
+#include <math.h>
 
 struct Student 
 {
-   std::string student_name;
-   std::string student_surname;
-   std::string index_number;
+   const std::string student_name;
+   const std::string student_surname;
+   const std::string index_number;
    int current_semester;
    float grades_average; 
+   auto to_string() const -> std::string;      
 
-   auto to_string() const -> std::string {
-      return ("Student name: " + student_name
-           + "\nStudent surname: " + student_surname
-           + "\nStudent index number: " + index_number
-           + "\nCurrent semester: " + std::to_string(current_semester)
-           + "\nGrades point average: " + std::to_string(grades_average)
-      );       
-   }
 };
 
+auto Student::to_string() const -> std::string 
+{
+   std::string Grades = std::to_string(grades_average);
+   std::string Semester = std::to_string(current_semester);
+
+  return (student_name + " " + student_surname + " (" + index_number 
+          +  "), semester "  + Semester + ": " + Grades);    
+}
 
 auto main() -> int
-{
-   Student my_details;
-   
-   my_details.student_name = "Michał";
-   my_details.student_surname = "Jastrzemski";
-   my_details.index_number = "s26245";
-   my_details.current_semester = 1;
-   my_details.grades_average = 4.57;
 
-   std::cout << my_details.to_string() << "\n";
+{
+   auto me = Student{"Michał", "Jastrzemski", "s26245", 1, 4.75};
+   std::cout << me.to_string() << "\n";
 }
